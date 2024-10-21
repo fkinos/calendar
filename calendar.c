37da8d3d6ddea666afc6e670370624e129cde5fb
Felipe Kinoshita <kinofhek@gmail.com>
2024-10-21 01:14:31 -0300

    Allow to go the next/previous months and years

--- calendar.c
+++ calendar.c
@@ -269,6 +269,32 @@ static int months_days[12] = {
   31, 28, 31, 20, 31, 30, 31, 31, 30, 31, 30, 31
 };
 
+static int current_month, actual_month, current_year, actual_year;
+
+int
+year ()
+{
+  struct tm *tm;
+  time_t t;
+
+  t = time (NULL);
+  tm = localtime (&t);
+
+  return tm->tm_year;
+}
+
+int
+month ()
+{
+  struct tm *tm;
+  time_t t;
+
+  t = time (NULL);
+  tm = localtime (&t);
+
+  return tm->tm_mon;
+}
+
 int
 weekday (int day)
 {
@@ -279,6 +305,8 @@ weekday (int day)
   t = time (NULL);
   tm = localtime (&t);
 
+  tm->tm_year = current_year;
+  tm->tm_mon = current_month;
   tm->tm_mday = day;
 
   nt = mktime(tm);
@@ -292,6 +320,7 @@ draw ()
 {
   int i, j;
   time_t t;
+  time_t nt;
   struct tm *tm;
   char heading[256];
   int calendar[6][7];
@@ -302,9 +331,14 @@ draw ()
   t = time (0);
   tm = localtime (&t);
 
+  tm->tm_yday = current_year;
+  tm->tm_mon = current_month;
+  nt = mktime (tm);
+  tm = localtime (&nt);
+
   today = tm->tm_mday;
   start = weekday(1) + 1;
-  days = months_days[tm->tm_mon];
+  days = months_days[current_month];
 
   clear_screen (pixels, WHITE);
 
@@ -338,11 +372,44 @@ draw ()
       if (calendar[j][i] != 0) {
         char day_string[3];
         int color = calendar[j][i] == today ? RED : BLACK;
+        if (current_month != actual_month || current_year != actual_year)
+          color = BLACK;
 
         snprintf (day_string, 3, "%2d", calendar[j][i]);
         draw_string (pixels, 8 + (i * 20), 44 + (j * 15), day_string, color);
       }
     }
+  }
+}
+
+void
+handle_keys (SDL_Event e)
+{
+  int sym = e.key.keysym.sym;
+  int offset = 0;
+  int new_current_month = 0;
+  int last_current_month = 0;
+
+  switch (sym) {
+  case SDLK_LEFT:
+    offset = -1;
+    break;
+  case SDLK_RIGHT:
+    offset = 1;
+    break;
+  }
+
+  last_current_month = current_month;
+
+  new_current_month = (current_month - 1 + (offset % 12) + 12) % 12 + 1;
+  current_month = new_current_month;
+
+  if (last_current_month == 1 && new_current_month == 12) {
+    current_year -= 1;
+  }
+
+  if (last_current_month == 12 && new_current_month == 1) {
+    current_year += 1;
   }
 }
 
@@ -354,6 +421,11 @@ main (int argc, char *argv[])
   if (!init ())
     fprintf (stderr, "INIT ERROR: FAILURE\n");
   set_zoom (1, 1);
+
+  current_year = year ();
+  actual_year = year ();
+  current_month = month ();
+  actual_month = month ();
 
   clear_screen (pixels, BLACK);
 
@@ -376,6 +448,7 @@ main (int argc, char *argv[])
       switch(e.type) {
       case SDL_QUIT: emu_end (); break;
       case SDL_WINDOWEVENT: reqdraw = 1; break;
+      case SDL_KEYDOWN: handle_keys(e); break;
       }
     }
 
